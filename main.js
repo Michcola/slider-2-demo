c58906b293cce63f632c6459f230b5e9e785373b
mich <843298603@qq.com>
2019-03-30 20:37:37 +0800

    第一个版本

--- main.js
+++ main.js
@@ -1,63 +1,23 @@
-let n
-init()//初始化
-setInterval(() => {
-    makeLeave(getImage(n))
+$('.images > img:nth-child(1)').addClass('current')
+$('.images > img:nth-child(2)').addClass('enter')
+$('.images > img:nth-child(3)').addClass('enter')
+let n = 1
+setInterval(()=>{
+  $(`.images > img:nth-child(${x(n)})`).removeClass('current').addClass('leave')
     .one('transitionend', (e)=>{
-        makeEnter($(e.currentTarget))
+      $(e.currentTarget).removeClass('leave').addClass('enter')
     })
-    makeCurrent(getImage(n+1))
-n += 1
+  $(`.images > img:nth-child(${x(n+1)})`).removeClass('enter').addClass('current')
+  n += 1
 },3000)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// 下面可以不看
-function getImage(n) {
-    return $(`.images > img:nth-child(${x(n)})`)
-}
 function x(n){
-    if(n>5){
-        n = n%5
-        if(n===0){
-            n =5
-        }
+  if(n>3){
+    n = n%3
+    if (n===0){
+      n =3
     }
-    return n
-}
-
-function init() {
-    n=1
-    $(`.images > img:nth-child(${n})`).addClass('current')
-    .siblings().addClass('enter')
-}
-    
-function makeCurrent($node) {
-    $node.removeClass('enter leave').addClass('current')
-}
-function makeLeave($node) {
-    $node.removeClass('enter current').addClass('leave')
-    return $node
-}
-function makeEnter($node) {
-    $node.removeClass('leave current').addClass('enter')   
+  } // n = 1 2 3
+  return n
 }
